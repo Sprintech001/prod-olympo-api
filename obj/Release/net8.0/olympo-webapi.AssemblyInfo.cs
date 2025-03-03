b445e686692ac5c3e3b00d5ba0147e1f951a6098
Thomaz Rodrigues <thomazrodrigues668@gmail.com>
2025-03-03 19:27:42 -0300

    build 02

--- obj/Release/net8.0/olympo-webapi.AssemblyInfo.cs
+++ obj/Release/net8.0/olympo-webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("olympo-webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a373d2e659c728dfadc4513429b53e33eaed0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b9dc8fa96ed3818409b22411e246c66a0fde33")]
 [assembly: System.Reflection.AssemblyProductAttribute("olympo-webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("olympo-webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
